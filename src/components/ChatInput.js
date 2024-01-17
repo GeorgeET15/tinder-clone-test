68c41ce5d57591e9e4b85f8496cde9ba00e81dc8
GeorgeET15 <ironmanrocks353@gmail.com>
2024-01-18 04:04:54 +0530

    tinder-clone-test

---
+++ src/components/ChatInput.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
+const ChatInput = ({
+  user,
+  clickedUser,
+  getUserMessages,
+  getClickedUsersMessages,
+}) => {
+  const [textArea, setTextArea] = useState("");
+  const userId = user?.user_id;
+  const clickedUserId = clickedUser?.user_id;
+
+  const addMessage = async () => {
+    const message = {
+      timestamp: new Date().toISOString(),
+      from_userId: userId,
+      to_userId: clickedUserId,
+      message: textArea,
+    };
+
+    try {
+      await axios.post("http://localhost:8000/message", { message });
+      getUserMessages();
+      getClickedUsersMessages();
+      setTextArea("");
+    } catch (error) {
+      console.error("Error adding message:", error);
+    }
+  };
+
+  return (
+    <div className="chat-input">
+      <textarea
+        placeholder="Type your message..."
+        value={textArea}
+        onChange={(e) => setTextArea(e.target.value)}
+      />
+      <button className="primary-button" onClick={addMessage}>
+        Send
+      </button>
+    </div>
+  );
+};
+
+export default ChatInput;
