5a2d2c68c7f14742311271e0da2fdd8e45d44f38
GeorgeET15 <ironmanrocks353@gmail.com>
2024-01-21 11:33:10 +0530

    bcryptjs

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 const { v4: uuidv4 } = require("uuid");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const uri = process.env.URI;
 const app = express();
